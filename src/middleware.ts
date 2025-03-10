ee114335c30c456a9701ebf0f02f997b004189b4
Prashant Shrestha <prashantshrestha700@gmail.com>
2025-03-10 21:03:02 +0530

    chnaged middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
   console.log(request.url, "middleware");
 
   const sessionCookie = getSessionCookie(request); // Optionally pass config as the second argument if cookie name or prefix is customized.
-  if (!sessionCookie) {
+  if (!sessionCookie || sessionCookie == null) {
     console.log(request.url, "middleware2");
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
